cfe2527c482e08c9f92f8d9c4611ed101f6e4865
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-08-18 13:15:08 +0300

    Last Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["react-chat-engine-advanced"],
+    },
+  },
   plugins: [react()],
   server: {
     host: "127.0.0.1",
